f70ee63cce764116917192a5ca5afe7eb0407484
k1lLr3AlL <31995128+k1lLr3AlL@users.noreply.github.com>
2018-05-07 21:33:25 +0300

    fix mistakes

--- class_dict.py
+++ class_dict.py
@@ -6,24 +6,25 @@ class DirDict(collections.MutableMapping):
     def __init__(self, path):
         self.path = path
 
+    # Если такого файла нет вызваем исключение, иначе возвращаем содержимое
     def __getitem__(self, key):
+        try:
+            with open(os.path.join(self.path, key)) as handler:
+                return handler.read()
+        except FileNotFoundError:
+            raise KeyError
 
-        for file in os.listdir(self.path):
-            if file.split('.')[0] == key:
-                with open(self.path + file) as handler:
-                    return handler.read().replace('\n', ' ')
-
+    # Перезаписываем существующий файл, если такого файла нет, то создаем его
     def __setitem__(self, key, value):
+        with open(os.path.join(self.path, key), 'w') as handler:
+            handler.write(value)
 
-        for file in os.listdir(self.path):
-            if file.split('.')[0] == key:
-                with open(self.path + file, 'w') as handler:
-                    handler.write(value)
-
+    # Удаляем файл, если его нет вызываем исключение
     def __delitem__(self, key):
-        for file in os.listdir(self.path):
-            if file.split('.')[0] == key:
-                os.remove(self.path + file)
+        try:
+            os.remove(os.path.join(self.path, key))
+        except FileNotFoundError:
+            raise KeyError
 
     def __iter__(self):
         return iter(self.__dict__)
@@ -31,75 +32,60 @@ class DirDict(collections.MutableMapping):
     def __len__(self):
         return len(os.listdir(self.path))
 
+    # Метод возвращает генератор, который возвращает сод-ое файлов
     def values(self):
         for file in os.listdir(self.path):
-            with open(self.path + file) as handler:
-                yield handler.read().replace('\n', ' ')
+            with open(os.path.join(self.path, file)) as handler:
+                yield handler.read()
 
+    # Метод возвращает генератор, который возвращает имя файлов
     def keys(self):
         for file in os.listdir(self.path):
-            yield file.split('.')[0]
+            yield file
 
+    # Метод возвращает генератор, который возвращает имя файла - содержимое
     def items(self):
         for file in os.listdir(self.path):
-            with open(self.path + file) as handler:
-                yield (file.split('.')[0], handler.read().replace('\n', ' '))
+            with open(os.path.join(self.path, file)) as handler:
+                yield (file, handler.read())
 
+    # Метод удаляет все содержимое директории
     def clear(self):
         for file in os.listdir(self.path):
-            os.remove(self.path + file)
+            os.remove(os.path.join(self.path, file))
 
+    # Метод вернет сод-ое файла, если такого файла нет, то вернет default
     def get(self, key, default=None):
-        for file in os.listdir(self.path):
-            if file.split('.')[0] == key:
-                with open(self.path + file) as handler:
-                    return handler.read().replace('\n', ' ')
-        else:
+        try:
+            return self[key]
+        except KeyError:
             return default
 
+    # Метод удаляет последний файл в директории
     def popitem(self):
-        os.remove(self.path + os.listdir(self.path)[-1])
+        os.remove(os.path.join(self.path, os.listdir(self.path)[-1]))
 
+    # Метод возвращает сод-ое файла если его нет, то содает и пишет в него default
     def setdefault(self, key, default=None):
-        for file in os.listdir(self.path):
-            if file.split('.')[0] == key:
-                with open(self.path + file) as handler:
-                    return handler.read().replace('\n', ' ')
+        try:
+            return self[key]
+        except KeyError:
+            self[key] = default
 
-        else:
-            with open(self.path + key, 'w') as handler:
-                handler.write(default)
+    # Обновляет директорию
+    def update(self, new_dict):
+        for key in new_dict.keys():
+            self[key] = new_dict[key]
 
-    def update(*args):  # self находится в args[0]
-        print(args[0].path, args[1].path)
-        for key1 in os.listdir(args[1].path):
-            for key0 in os.listdir(args[0].path):
-                if key1 == key0:
-                    for new_key1 in os.listdir(args[1].path):
-                        for new_key0 in os.listdir(args[0].path):
-                            if new_key1 == new_key0:
-                                with open(args[0].path + new_key0, 'w') as handler0:
-                                    with open(args[1].path + new_key1) as handler1:
-                                        handler0.write(handler1.read())
-                                        break
-
-                        else:
-                            with open(args[0].path + new_key1, 'w') as handler0:
-                                with open(args[1].path + new_key1) as handler1:
-                                    handler0.write(handler1.read())
-        else:
-            return None
-
+    # Копирует содержимое текущей директории в указанную
     def copy(self, new_path):
         if os.path.exists(new_path):
-            for file in os.listdir(self.path):
-                with open(self.path + file) as handler1:
-                    with open(new_path + file, 'w') as handler2:
-                        handler2.write(handler1.read())
-            return DirDict(new_path)
+            new_dict = DirDict(new_path)
+            for key, data in self.items():
+                new_dict[key] = data
+            return new_dict
         else:
             return None
-
 
 
 
